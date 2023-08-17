f93267413a534a2f1eefcaab6d0d5e3fa1cfca8f
tauseef24 <tauseef242003@gmail.com>
2023-08-17 20:12:18 +0530

    updated

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+const db = require('./models')
+
+//Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
+const usersRouter = require("./routes/Users");
+app.use("/auth", usersRouter);
+const likesRouter = require("./routes/Likes");
+app.use("/likes", likesRouter);
+
+db.sequelize
+    .sync()
+    .then(() => {
+        app.listen(process.env.PORT || 3001, () => {
+            console.log("Server running on port 3001");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
